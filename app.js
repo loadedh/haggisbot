4478d5b531c54835fb5eb1fbd48a33a805b36c35
Kieron Hagg <loadedh@gmail.com>
2016-10-18 16:53:12 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// let userWeather, userCity;
-//
-// const api = 'http://api.openweathermap.org/data/2.5/weather?' + '&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric&';
-// function() {
-//   userWeather = api + userCity;
-//   return userWeather;
-// }
-
 const app = express();
 const port = process.env.PORT || 1313;
 
@@ -41,8 +33,9 @@ app.post('/weather', function(req, res, next) {
   const userName = req.body.user_name;
   const messageText = req.body.text;
   const getCity = messageText.match(/[^weather ]/gi);
+  const userWeather = 'http://api.openweathermap.org/data/2.5/weather?q='+ [...getCity] +'&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric';
   const botPayLoad = {
-    text: 'Hello ' + userName + ', the weather for London is ' + 'http://api.openweathermap.org/data/2.5/weather?q='+ getCity +'&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric'
+    text: `Hello ${userName}, the weather for ${[...getCity]} is ${userWeather}`
     // text: 'Hello ' + userName + ', the weather for ' + userCity + ' is ' + userWeather.weather.clouds + ' : ' + userWeather.main.temp
   };
 
