c870a2891e7da7601b73c922aff58827b5b46b89
Kieron Hagg <loadedh@gmail.com>
2016-10-18 12:05:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ let weather, apiKey = 'APPID=fc2a5047efd117936135c68fe985dcf6';
 const app = express();
 const port = process.env.PORT || 1313;
 
-function weatherSetUp() {
-  loadJson('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=fc2a5047efd117936135c68fe985dcf6', getWeatherData)
-}
-function getWeatherData(data) {
-  weather = data;
-}
-
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -38,15 +31,15 @@ app.post('/hello', function(req, res, next) {
     }
 });
 
-app.post('/weatherLondon', function(req, res, next) {
-  const userName = req.body.user_name;
-  const botPayLoad = {
-    text: 'Hello ' + userName + ', the temperature for London is ' + weather
-  };
-
-  if (userName !== 'slackbot') {
-    return res.status(200).json(botPayLoad);
-  } else {
-    return res.status(200).end();
-  }
-});
+// app.post('/weatherLondon', function(req, res, next) {
+//   const userName = req.body.user_name;
+//   const botPayLoad = {
+//     text: 'Hello ' + userName + ', the temperature for London is ' + weather
+//   };
+//
+//   if (userName !== 'slackbot') {
+//     return res.status(200).json(botPayLoad);
+//   } else {
+//     return res.status(200).end();
+//   }
+// });
