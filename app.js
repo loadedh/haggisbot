4baf34db09587b286765d928e1c0596f37b4bc68
Kieron Hagg <loadedh@gmail.com>
2016-10-18 13:38:34 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get('/', function(reg, res) {
-    res.status(200).send('The server is up!!');
+    res.status(200).send('App is live!!');
 });
 
 app.listen(port, function() {
