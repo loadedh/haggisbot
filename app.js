fccc8a0b6c42906eb92bb5b93e81a9d976bb6bc1
Kieron Hagg <loadedh@gmail.com>
2016-10-18 17:41:38 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post('/weather', function(req, res, next) {
   const userName = req.body.user_name;
   const messageText = req.body.text;
   const getCity = messageText.split('weather ');
-  const userWeather = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`);
+  const userWeather = `http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`;
   const botPayLoad = {
     text: `Hello ${userName}, the weather for ${getCity[1]} is: '\n'${userWeather}`
     // text: 'Hello ' + userName + ', the weather for ' + userCity + ' is ' + userWeather.weather.clouds + ' : ' + userWeather.main.temp
