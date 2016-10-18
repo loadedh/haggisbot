0ddb29c04651ff0cd71e8413a4d38f01c7426f78
Kieron Hagg <loadedh@gmail.com>
2016-10-18 11:55:06 +0100

    Added weather output
    bot will respond to requests for the weather in London with the current temperature

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+let weather, apiKey = 'APPID=fc2a5047efd117936135c68fe985dcf6';
 const app = express();
 const port = process.env.PORT || 1313;
+
+function weatherSetUp() {
+  loadJson('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=fc2a5047efd117936135c68fe985dcf6', getWeatherData)
+}
+function getWeatherData(data) {
+  weather = data;
+}
+
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -28,3 +37,16 @@ app.post('/hello', function(req, res, next) {
         return res.status(200).end();
     }
 });
+
+app.post('/weatherLondon', function() {
+  const userName = req.body.user_name;
+  const botPayLoad = {
+    text: 'Hello ' + userName + ', the temperature for London is ' + weather
+  };
+
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayLoad);
+  } else {
+    return res.status(200).end();
+  }
+})
