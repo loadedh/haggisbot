123987853a540e9aefd43bec4136897cfb3a0032
Kieron Hagg <loadedh@gmail.com>
2016-10-19 14:17:46 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -34,31 +34,31 @@ app.post('/weather', function(req, res, next) {
     const messageText = req.body.text;
     const getCity = messageText.split('-weather ');
 
-    function getJSON(url, callback) {
+    const getJSON = function(url, callback) {
         const Httpreq = new XMLHttpRequest();
         Httpreq.open("GET", url, false);
         Httpreq.responseType = 'json';
         Httpreq.onload = function() {
-          const status = Httpreq.status;
-          if (status == 200) {
-            callback(null, Httpreq.response);
-          } else {
-            callback(status)
-          }
+            const status = Httpreq.status;
+            if (status == 200) {
+                callback(null, Httpreq.response);
+            } else {
+                callback(status)
+            }
         }
         Httpreq.send();
     };
 
-    const json_obj = getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`,
-    function(err, data) {
-      if (err != null) {
-        console.log('Something went wrong')
-      } else {
-        const botPayLoad = {
-            text: `Hello ${userName}, the weather for ${getCity[1]} is: \n TEMP - ${json_obj.main.temp} \n WEATHER - ${json_obj.weather.description}`
-        };
-      }
-    });
+    getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`,
+        function(err, data) {
+            if (err != null) {
+                console.log('Something went wrong')
+            } else {
+                const botPayLoad = {
+                    text: `Hello ${userName}, the weather for ${getCity[1]} is: \n TEMP - ${data.main.temp} \n WEATHER - ${data.weather.description}`
+                };
+            }
+        });
     // const json_obj = JSON.parse(Get(`http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`))
 
     if (userName !== 'slackbot') {
