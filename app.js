710e3991afba440e7e6253636e157dbf524e9256
Kieron Hagg <loadedh@gmail.com>
2016-10-19 11:06:26 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.get('/', function(reg, res) {
 });
 
 app.listen(port, function() {
-    console.log('We are listening to the port: ' + port);
+    console.log(`We are listening to the port: ${port}`);
 });
 
 app.post('/hello', function(req, res, next) {
     const userName = req.body.user_name;
     const botPayLoad = {
-        text: 'Hello ' + userName + ', this is the Haggis slack channel. ^_^ Welcome and have fun! ^_^ '
+        text: `Hello ${userName}, this is the Haggis slack channel. ^_^ Welcome and have fun! ^_^`
     };
 
     if (userName !== 'slackbot') {
@@ -30,18 +30,18 @@ app.post('/hello', function(req, res, next) {
 });
 
 app.post('/weather', function(req, res, next) {
-  const userName = req.body.user_name;
-  const messageText = req.body.text;
-  const getCity = messageText.split('weather ');
-  const userWeather = `http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`;
-  const botPayLoad = {
-    text: `Hello ${userName}, the weather for ${getCity[1]} is: \n ${userWeather}`
-    // text: 'Hello ' + userName + ', the weather for ' + userCity + ' is ' + userWeather.weather.clouds + ' : ' + userWeather.main.temp
-  };
+    const userName = req.body.user_name;
+    const messageText = req.body.text;
+    const getCity = messageText.split('-weather ');
+    const userWeather = $.getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`, function(data));
+    const botPayLoad = {
+        text: `Hello ${userName}, the weather for ${getCity[1]} is: \n ${data.main.temp}`
+            // text: 'Hello ' + userName + ', the weather for ' + userCity + ' is ' + userWeather.weather.clouds + ' : ' + userWeather.main.temp
+    };
 
-  if (userName !== 'slackbot') {
-    return res.status(200).json(botPayLoad);
-  } else {
-    return res.status(200).end();
-  }
+    if (userName !== 'slackbot') {
+        return res.status(200).json(botPayLoad);
+    } else {
+        return res.status(200).end();
+    }
 });
