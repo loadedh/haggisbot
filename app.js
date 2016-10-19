1440fa5a0bc03c0660208bc9dc3e4b3d2572177b
Kieron Hagg <loadedh@gmail.com>
2016-10-19 14:37:58 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -34,36 +34,29 @@ app.post('/weather', function(req, res, next) {
     const messageText = req.body.text;
     const getCity = messageText.split('-weather ');
 
-    const getJSON = function(url, callback) {
-        const Httpreq = new XMLHttpRequest();
-        Httpreq.open("GET", url, false);
-        Httpreq.responseType = 'json';
-        Httpreq.onload = function() {
-            const status = Httpreq.status;
-            if (status == 200) {
-                callback(null, Httpreq.response);
-            } else {
-                callback(status)
-            }
+    function httpGetAsync(theUrl, callback) {
+        const xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                callback(xmlHttp.responseText);
         }
-        Httpreq.send();
-    };
-
-    getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`,
+        xmlHttp.open("GET", theUrl, true); // true for asynchronous
+        xmlHttp.send(null);
+    }
+    const botPayLoad = httpGetAsync(`http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`,
         function(err, data) {
             if (err != null) {
                 console.log('Something went wrong')
             } else {
-                const botPayLoad = {
-                    text: `Hello ${userName}, the weather for ${getCity[1]} is: \n TEMP - ${data.main.temp} \n WEATHER - ${data.weather.description}`
-                };
-            }
-        });
-    // const json_obj = JSON.parse(Get(`http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`))
+                text: `Hello ${userName}, the weather for ${getCity[1]} is: \n TEMP - ${data.main.temp} \n WEATHER - ${data.weather.description}`
+            };
+        }
+    });
+// const json_obj = JSON.parse(Get(`http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`))
 
-    if (userName !== 'slackbot') {
-        return res.status(200).json(botPayLoad);
-    } else {
-        return res.status(200).end();
-    }
+if (userName !== 'slackbot') {
+    return res.status(200).json(botPayLoad);
+} else {
+    return res.status(200).end();
+}
 });
