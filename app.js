0eff106cf49ffaefe8893bec052f633a5d6ae8af
Kieron Hagg <loadedh@gmail.com>
2016-10-20 09:51:49 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -35,22 +35,15 @@ app.post('/weather', function(req, res, next) {
     const getCity = messageText.split('weather ');
     const apiResponse = `http://api.openweathermap.org/data/2.5/weather?q=${getCity[1]}&APPID=fc2a5047efd117936135c68fe985dcf6&units=metric`;
     const botPayLoad = http.get(apiResponse, (res) => {
-        return {
+      {
             text: `Hello ${userName}, here is the weather for ${getCity}:
            \nTEMP - ${res.main.temp}
            \nWEATHER - ${res.weather.description}
            \nWIND SPEED - ${res.wind.speed}`
-        };
+        }
+    }).on('error', (e) => {
+      console.log(`got error: ${e}`)
     });
-
-
-    //     $.getJSON(apiResponse, function(data) {
-    //        text: `Hello ${userName}, here is the weather for ${getCity}:
-    //     \nTEMP - ${data.main.temp}
-    //     \nWEATHER - ${data.weather.description}
-    //     \nWIND SPEED - ${data.wind.speed}`
-    //   })
-    // };
 
     if (userName !== 'slackbot') {
         return res.status(200).json(botPayLoad);
